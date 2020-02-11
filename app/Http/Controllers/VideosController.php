31b3841563673e81021d82005739492b1cff2dc9
Alexandre Vessoni <av.vessoni@gmail.com>
2020-02-11 18:42:16 -0300

    Refactory VideosController

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -3,62 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
+use App\Repositories\Videos;
+
 
 class VideosController extends Controller
 {
+    protected $videos;
+
+    public function __construct(Videos $videos)
+    {
+        $this->videos = $videos;
+    }
+
     public function index(){
-        $apiData = [
-            'baseUrl' => 'https://www.googleapis.com/youtube/v3/videos?',
-            'chart' => 'mostPopular',
-            'part' => 'snippet,statistics',
-            'type' => 'video',
-            'order' => 'viewCount',
-            'maxResults' => 12,
-            'fields'=> 'items(id,snippet,statistics)',
-             'q' => '',
-            'key' => 'AIzaSyCpwfozr6iK7gqrEL0IIPeVfY7ExGPNg_Y',
-        ];
 
-            $client = new Client([
-                'base_uri' => $apiData['baseUrl'].'chart='.$apiData['chart'].'&part='.$apiData['part'].'&type='.$apiData['type'].
-                '&order='.$apiData['order'].'&q='.$apiData['q'].'&maxResults='.$apiData['maxResults'].'&fields='.$apiData['fields'].'&key='.$apiData['key'],
-            ]);
-
-            $response = $client->request('GET', '');
-            $videos =  json_decode(trim($response->getBody()->getContents()), TRUE);
-
+            $videos = $this->videos->all();
 
             return view('videos.index', compact('videos'));
     }
 
     public function show($id){
 
-        $apiData = [
-            'baseUrl' => 'https://www.googleapis.com/youtube/v3/videos?',
-            'id' => $id,
-            'part' => 'snippet,statistics',
-            'type' => 'video',
-            'order' => 'viewCount',
-            'maxResults' => 12,
-            'fields'=> 'items(id,snippet,statistics)',
-             'q' => '',
-            'key' => 'AIzaSyCpwfozr6iK7gqrEL0IIPeVfY7ExGPNg_Y',
-        ];
-
-            $client = new Client([
-                'base_uri' => $apiData['baseUrl'].'id='.$apiData['id'].'&part='.$apiData['part'].'&type='.$apiData['type'].
-                '&order='.$apiData['order'].'&q='.$apiData['q'].'&maxResults='.$apiData['maxResults'].'&fields='.$apiData['fields'].'&key='.$apiData['key'],
-            ]);
-
-            $response = $client->request('GET', '');
-            $video =  json_decode(trim($response->getBody()->getContents()), TRUE);
-
-            $video = $video['items'][0];
-            // echo '<pre>';
-            // var_dump($video);
-            // echo '</pre>';
-            // return null;
+        $video = $this->videos->find($id);
 
             return view('videos.show', compact('video'));
     }
---
+++ app/Repositories/Videos.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repositories;
+use GuzzleHttp\Client;
+
+class Videos {
+
+    public function all(){
+        $apiData = [
+            'baseUrl' => 'https://www.googleapis.com/youtube/v3/videos?',
+            'chart' => 'mostPopular',
+            'part' => 'snippet,statistics',
+            'type' => 'video',
+            'order' => 'viewCount',
+            'maxResults' => 12,
+            'fields'=> 'items(id,snippet,statistics)',
+             'q' => '',
+            'key' => 'AIzaSyCpwfozr6iK7gqrEL0IIPeVfY7ExGPNg_Y',
+        ];
+
+            $client = new Client([
+                'base_uri' => $apiData['baseUrl'].'chart='.$apiData['chart'].'&part='.$apiData['part'].'&type='.$apiData['type'].
+                '&order='.$apiData['order'].'&q='.$apiData['q'].'&maxResults='.$apiData['maxResults'].'&fields='.$apiData['fields'].'&key='.$apiData['key'],
+            ]);
+
+            $response = $client->request('GET', '');
+            return json_decode(trim($response->getBody()->getContents()), TRUE);
+
+    }
+
+    public function find($id){
+
+
+        $apiData = [
+            'baseUrl' => 'https://www.googleapis.com/youtube/v3/videos?',
+            'id' => $id,
+            'part' => 'snippet,statistics',
+            'type' => 'video',
+            'order' => 'viewCount',
+            'maxResults' => 12,
+            'fields'=> 'items(id,snippet,statistics)',
+             'q' => '',
+            'key' => 'AIzaSyCpwfozr6iK7gqrEL0IIPeVfY7ExGPNg_Y',
+        ];
+
+            $client = new Client([
+                'base_uri' => $apiData['baseUrl'].'id='.$apiData['id'].'&part='.$apiData['part'].'&type='.$apiData['type'].
+                '&order='.$apiData['order'].'&q='.$apiData['q'].'&maxResults='.$apiData['maxResults'].'&fields='.$apiData['fields'].'&key='.$apiData['key'],
+            ]);
+
+            $response = $client->request('GET', '');
+            $video =  json_decode(trim($response->getBody()->getContents()), TRUE);
+
+            return $video['items'][0];
+    }
+
+}
